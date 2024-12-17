22ef1866adce8b123919d73dec0bb0461ef20d2b
James R T Bossert <james_bossert@ncahec.net>
2024-12-17 18:37:41 -0500

    2nd try

--- database/migrations/{timestamp}_create_longest_words_table.php
+++ database/migrations/{timestamp}_create_longest_words_table.php
@@ -1,1 +1,26 @@
- 
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        if (!Schema::hasTable('longest_words')) {
+            Schema::create('longest_words', function (Blueprint $table) {
+                $table->id();
+                $table->string('word');
+                $table->string('session_id');
+                $table->string('player_id');
+                $table->timestamps();
+            });
+        }
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('longest_words');
+    }
+}; 
